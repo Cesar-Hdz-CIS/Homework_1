f6fff26ef7b120bfa83dbbe9b8d38b6101d8a1fa
Cesar-Hdz_CIS <cohernan@cougarnet.uh.edu>
2021-09-18 00:28:24 -0500

    Homework_1

--- Problem1/main.py
+++ Problem1/main.py
@@ -10,5 +10,15 @@ Birth_Month = int(input('Month: '))
 Birth_Day = int(input('Day: '))
 Birth_Year = int(input('Year: '))
 
+Years = Curr_Year - Birth_Year - 1
+
+if Birth_Month < Curr_Month:
+    Years += 1
+elif Curr_Month == Birth_Month:
+    if Birth_Day <= Curr_Day:
+        Years += 1
+
+print('You are', Years, 'old.')
+
 if Curr_Day == Birth_Day and Curr_Month == Birth_Month:
     print('Happy Birthday!')
