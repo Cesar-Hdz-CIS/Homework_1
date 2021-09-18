84b2d8a5541bb8d21d42b2d2b6d54422e0518a3b
Cesar-Hdz_CIS <cohernan@cougarnet.uh.edu>
2021-09-17 23:57:07 -0500

    Homework_1

---
+++ Problem1/main.py
@@ -0,0 +1,11 @@
+# Cesar Hernandez 1835494
+
+print('Birthday Calculator')
+print('Current Day')
+Curr_Month = int(input("Month: "))
+Curr_Day = int(input('Day: '))
+Curr_Year = int(input('Year: '))
+print('Birthday')
+Birth_Month = int(input('Month: '))
+Birth_Day = int(input('Day: '))
+Birth_Year = int(input('Year: '))
