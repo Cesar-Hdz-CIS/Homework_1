7254deec5bf7fa02e23d8e967ff8ea8e12a1afa0
Cesar-Hdz_CIS <cohernan@cougarnet.uh.edu>
2021-09-17 23:31:40 -0500

    Homework_1

--- Zybooks 5_22/main.py
+++ Zybooks 5_22/main.py
@@ -7,10 +7,11 @@ print("Davy's auto shop services\n"
       "Car wax -- $12\n")
 Sum_services = 0
 services = {
-      'Oil change': 35,
-      'Tire rotation': 19,
-      'Car wash': 7,
-      'Car wax': 12
+    'Oil change': 35,
+    'Tire rotation': 19,
+    'Car wash': 7,
+    'Car wax': 12,
+    '-': 0
 }
 
 first_service = str(input("Select first service:\n"))
@@ -18,7 +19,22 @@ Sum_services += services[first_service]
 second_service = str(input("Select second service:\n"))
 Sum_services += services[second_service]
 
-print("Davy's auto shop invoice\n\n"
-      "Service 1:", first_service + str(","), "$" + str(services[first_service]),"\n"
-      "Service 2:", second_service + str(","), "$" + str(services[second_service]),"\n\n"
-      "Total: $" + str(Sum_services))
+print("\nDavy's auto shop invoice\n")
+
+if not services[first_service] == 0:
+    print("Service 1:", first_service + str(","), "$" + str(services[first_service]))
+else:
+    print('Service 1: No service')
+
+if not services[second_service] == 0:
+    print("Service 2:", second_service + str(","), "$" + str(services[second_service]))
+
+else:
+    print('Service 2: No service')
+
+print("\nTotal: $" + str(Sum_services))
+
+# print("Davy's auto shop invoice\n\n"
+#      "Service 1:", first_service + str(","), "$" + str(services[first_service]),"\n"
+#      "Service 2:", second_service + str(","), "$" + str(services[second_service]),"\n\n"
+#      "Total: $" + str(Sum_services))
