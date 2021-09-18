82606349fe9b40590e2e2d5aab565ca011e0fbb6
Cesar-Hdz_CIS <cohernan@cougarnet.uh.edu>
2021-09-18 00:04:48 -0500

    Homework_1

--- Problem1/main.py
+++ Problem1/main.py
@@ -9,3 +9,6 @@ print('Birthday')
 Birth_Month = int(input('Month: '))
 Birth_Day = int(input('Day: '))
 Birth_Year = int(input('Year: '))
+
+if Curr_Day == Birth_Day and Curr_Month == Birth_Month:
+    print('Happy Birthday!')
